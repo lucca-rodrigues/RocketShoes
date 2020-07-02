0d327741e57521ea69dbbdc4dbc36ea070765791
Lucas Rodrigues <lucasrod2008rigues@gmail.com>
2020-07-02 19:12:09 -0300

    header

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { MdShoppingBasket } from 'react-icons/md';
 
-import { Container } from './styles';
+import { Container, Cart } from './styles';
 import Logo from '../../Assets/logo.svg';
 
 function Header() {
   return (
     <Container>
-      <img src={Logo} alt="logo" />
+      <Link to="/">
+        <img src={Logo} alt="logo" />
+      </Link>
+      <Cart to="/cart">
+        <div>
+          <strong>Meu carrinho</strong>
+          <span>3 itens</span>
+        </div>
+        <MdShoppingBasket size={36} color="#fff" />
+      </Cart>
     </Container>
   );
 }
--- src/Pages/Cart/index.js
+++ src/Pages/Cart/index.js
@@ -1,9 +1,68 @@
 import React from 'react';
+import {
+  MdRemoveCircleOutline,
+  MdAddCircleOutline,
+  MdDelete,
+} from 'react-icons/md';
 
-// import { Container } from './styles';
+import { Container, ProductTable, Total } from './styles';
 
 function Cart() {
-  return <div />;
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>PRODUTO</th>
+            <th>QTD</th>
+            <th>SUBTOTAL</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img
+                src="https://static.netshoes.com.br/produtos/tenis-asics-gel-rocket-9-masculino/80/D18-4432-080/D18-4432-080_detalhe1.jpg?ts=1581533003?ims=240x240"
+                alt=""
+              />
+            </td>
+            <td>
+              <strong>Tênis</strong>
+              <span>R$ 129,90</span>
+            </td>
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline size={20} color="#9159c1" />
+                </button>
+                <input type="number" readOnly value={1} />
+                <button type="button">
+                  <MdAddCircleOutline size={20} color="#9159c1" />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>R$ 258,90</strong>
+            </td>
+            <td>
+              <button type="button">
+                <MdDelete size={20} color="#9159c1" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+      <footer>
+        <button type="button">Finalizar pedido</button>
+        <Total>
+          <span>TOTAL</span>
+          <strong>R$ 1920,28</strong>
+        </Total>
+      </footer>
+    </Container>
+  );
 }
 
 export default Cart;
